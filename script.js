e3598b6a899b519ed7af47c9fe0744960e154ef3
Jahangir Alam <jahangir.alam@ap.averydennison.com>
2024-09-06 16:00:58 +0600

    add jarvis voice and scracth account

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new SpeechRecognition();
 const start_btn = document.getElementById('btn') ; 
 
-//hi
+const ai_say_okey = new SpeechSynthesisUtterance("Okey");
+const ai_say_wellcome = new SpeechSynthesisUtterance("Welcome to this tutorial!");
+
+  // Select a voice
+  const voices = speechSynthesis.getVoices();
+  ai_say_okey.voice = voices[0]; // Choose a specific voice
+
 
 
 recognition.onstart = function() {
@@ -23,18 +29,27 @@ recognition.onresult = function(event) {
 if (command.includes('jarvis')) {
     console.log(command)
     if (command.includes("open youtube")) {
+        speechSynthesis.speak(ai_say_okey );
         window.open("https://www.youtube.com", "_blank");
     } else if (command.includes("open facebook")) {
+        speechSynthesis.speak(ai_say_okey );
         window.open("https://www.facebook.com", "_blank");
     } else if (command.includes("open github")) {
         window.open("https://www.github.com", "_blank");
+        speechSynthesis.speak(ai_say_okey );
     } else if (command.includes("open my website")) {
+        speechSynthesis.speak(ai_say_okey );
         window.open("https://tanjim-tawhid.github.io/Protfolio/", "_blank");  // Replace with your website
     } else if (command.includes("open cd")) {
+        speechSynthesis.speak(ai_say_okey );
         window.open("https://www.codingal.com/bn-BD/student/dashboard/", "_blank");
     } else if (command.includes("open ca")) {
+        speechSynthesis.speak(ai_say_okey );
         window.open("https://chatgpt.com/?sso=", "_blank");
-    } 
+    } else if (command.includes("open sp") || command.includes("open nsp")){
+        speechSynthesis.speak(ai_say_okey );
+        window.open("https://scratch.mit.edu/", "_blank");
+    }
      else {
         document.getElementById('status').textContent = "Command not recognized.";
     }
